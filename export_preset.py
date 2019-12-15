731bf966ecdf92ec16aca18ebcac50e2d5e0e8af
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2019-12-15 17:06:29 +0300

    Temporary files for pure blender export presets.

---
+++ export_preset.py
@@ -0,0 +1,33 @@
+import bpy
+
+def export_fbx(preset):
+    filename = preset
+    print(filename)
+    preset_path = "D:\\"
+    print(preset_path)
+
+    if preset_path:
+        filepath = "D:\\test.py"
+
+        if filepath:
+            class Container(object):
+                __slots__ = ('__dict__',)
+
+            op = Container()
+            file = open(filepath, 'r')
+
+            # storing the values from the preset on the class
+            for line in file.readlines()[3::]:
+                exec(line, globals(), locals())
+
+            # change preset parameters
+            op.filepath = "D:\\test.obj"
+            # pass class dictionary to the operator
+            kwargs = op.__dict__
+            bpy.ops.export_scene.obj(**kwargs)
+        else:
+            raise FileNotFoundError(filepath)
+
+
+if __name__ == '__main__':
+    export_fbx('pure_fbx')
